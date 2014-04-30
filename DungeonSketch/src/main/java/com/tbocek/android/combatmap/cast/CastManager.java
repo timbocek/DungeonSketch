47c6760495835ec6a97e4e04e994522c50e8fcfc
tbocek <tim.bocek@gmail.com>
2014-04-30 10:23:42 -0700

    Add a little more copypaste chromecast scaffolding.  Dear god this is a log of boilerplate.

--- DungeonSketch/src/main/java/com/tbocek/android/combatmap/cast/CastManager.java
+++ DungeonSketch/src/main/java/com/tbocek/android/combatmap/cast/CastManager.java
@@ -11,6 +11,7 @@ import com.google.android.gms.cast.CastMediaControlIntent;
  * Created by tbocek on 4/29/14.
  */
 public class CastManager {
+    private static final String TAG = "CastManager";
     private MediaRouter mMediaRouter;
     private MediaRouteSelector mMediaRouteSelector;
     private CastDevice mSelectedDevice;
@@ -49,12 +50,95 @@ public class CastManager {
             mSelectedDevice = CastDevice.getFromBundle(info.getExtras());
             String routeId = info.getId();
 
+            Cast.CastOptions.Builder apiOptionsBuilder = Cast.CastOptions
+                    .builder(mSelectedDevice, mCastClientListener);
+
+            mApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Cast.API, apiOptionsBuilder.build())
+                    .addConnectionCallbacks(mConnectionCallbacks)
+                    .addOnConnectionFailedListener(mConnectionFailedListener)
+                    .build();
+            mApiClient.connect();
 
         }
 
         @Override
         public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo info) {
             mSelectedDevice = null;
+            teardown();
         }
     };
+
+    private GoogleApiClient.ConnectionCallbacks mConnectionCallbacks = new
+            GoogleApiClient.ConnectionCallbacks() {
+        @Override
+        public void onConnected(Bundle connectionHint) {
+            if (mWaitingForReconnect) {
+                mWaitingForReconnect = false;
+                reconnectChannels();
+            } else {
+                try {
+                    Cast.CastApi.launchApplication(mApiClient, "YOUR_APPLICATION_ID", false)
+                            .setResultCallback(
+                                    new ResultCallback<Cast.ApplicationConnectionResult>() {
+                                        @Override
+                                        public void onResult(Cast.ApplicationConnectionResult result) {
+                                            Status status = result.getStatus();
+                                            if (status.isSuccess()) {
+                                                ApplicationMetadata applicationMetadata =
+                                                        result.getApplicationMetadata();
+                                                String sessionId = result.getSessionId();
+                                                String applicationStatus = result.getApplicationStatus();
+                                                boolean wasLaunched = result.getWasLaunched();
+                                            } else {
+                                                teardown();
+                                            }
+                                        }
+                                    });
+
+                } catch (Exception e) {
+                    Log.e(TAG, "Failed to launch application", e);
+                }
+            }
+        }
+
+        @Override
+        public void onConnectionSuspended(int cause) {
+            mWaitingForReconnect = true;
+        }
+    };
+
+    private GoogleApiClient.OnConnectionFailedListener mConnectionFailedListener = new
+    GoogleApiClient.OnConnectionFailedListener() {
+        @Override
+        public void onConnectionFailed(ConnectionResult result) {
+            teardown();
+        }
+    };
+
+    mCastClientListener = new Cast.Listener() {
+        @Override
+        public void onApplicationStatusChanged() {
+            if (mApiClient != null) {
+                Log.d(TAG, "onApplicationStatusChanged: "
+                        + Cast.CastApi.getApplicationStatus(mApiClient));
+            }
+        }
+
+        @Override
+        public void onVolumeChanged() {
+            if (mApiClient != null) {
+                Log.d(TAG, "onVolumeChanged: " + Cast.CastApi.getVolume(mApiClient));
+            }
+        }
+
+        @Override
+        public void onApplicationDisconnected(int errorCode) {
+            teardown();
+        }
+    };
+
+    private void teardown() {
+
+    }
 }
