65561e217b57ada673cbf554c2014636013eff73
tbocek <tim.bocek@gmail.com>
2015-04-02 12:05:36 -0700

    Selection moving snaps to grid.

--- DungeonSketch/src/main/java/com/tbocek/android/combatmap/view/interaction/MoveSelectionInteractionMode.java
+++ DungeonSketch/src/main/java/com/tbocek/android/combatmap/view/interaction/MoveSelectionInteractionMode.java
@@ -3,12 +3,15 @@ package com.tbocek.android.combatmap.view.interaction;
 import android.view.MotionEvent;
 
 import com.tbocek.android.combatmap.model.primitives.CoordinateTransformer;
+import com.tbocek.android.combatmap.model.primitives.PointF;
 import com.tbocek.android.combatmap.view.CombatView;
 
 /**
  * Created by tbocek on 10/29/14.
  */
-public class MoveSelectionInteractionMode extends CombatViewInteractionMode {
+public class MoveSelectionInteractionMode extends BaseDrawInteractionMode {
+
+    private PointF mLastSnappedPoint;
 
     /**
      * Constructor.
@@ -24,6 +27,7 @@ public class MoveSelectionInteractionMode extends CombatViewInteractionMode {
     public boolean onDown(final MotionEvent e) {
         super.onDown(e);
         this.getView().beginBatchMove();
+        mLastSnappedPoint = getScreenSpacePoint(e);
         return true;
     }
 
@@ -31,12 +35,20 @@ public class MoveSelectionInteractionMode extends CombatViewInteractionMode {
     public boolean onScroll(final MotionEvent e1, final MotionEvent e2,
                             final float distanceX, final float distanceY) {
         CoordinateTransformer t = this.getView().getWorldSpaceTransformer();
+
+        PointF startPoint = this.getScreenSpacePoint(e1);
+        PointF newPoint = this.getScreenSpacePoint(e2);
+
+        float snappedDistanceX = mLastSnappedPoint.x - newPoint.x;
+        float snappedDistanceY = mLastSnappedPoint.y - newPoint.y;
+        mLastSnappedPoint = newPoint;
+
         if (this.getNumberOfFingers() == 1) {
             getView().getSelection().setTemporaryOffset(
-                    t.screenSpaceToWorldSpace(e2.getX() - e1.getX()),
-                    t.screenSpaceToWorldSpace(e2.getY() - e1.getY()),
-                    t.screenSpaceToWorldSpace(distanceX),
-                    t.screenSpaceToWorldSpace(distanceY));
+                    t.screenSpaceToWorldSpace(newPoint.x - startPoint.x),
+                    t.screenSpaceToWorldSpace(newPoint.y - startPoint.y),
+                    t.screenSpaceToWorldSpace(snappedDistanceX),
+                    t.screenSpaceToWorldSpace(snappedDistanceY));
             getView().refreshMap();
             return true;
         } else {
